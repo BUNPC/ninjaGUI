7dd28d11f6eb4cf197a181fc01d0dbeffbc176fc
Antonio Ortega <aortegam@bu.edu>
2020-12-24 11:59:11 -0500

    Revamped serial port

--- device_functions/fNIRS1kv1/fNIRS1k_FlushBuffer.m
+++
@@ -1,8 +0,0 @@
-function fNIRS1k_FlushBuffer(s)
-%% Just a basic call to flush the data buffer
-% Flush Buffer
-ba = s.BytesAvailable;
-while ba > 0
-    fread(s,ba,'uchar');
-    ba = s.BytesAvailable;
-end
