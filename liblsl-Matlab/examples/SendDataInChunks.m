af2cb45ab0c8813baf1ec0c7120887ddb1a81207
Antonio Ortega <aortegam@bu.edu>
2022-07-22 12:45:19 -0400

    Changed a minus sign
    For dOD visualization change of reference

--- liblsl-Matlab/examples/SendDataInChunks.m
+++ liblsl-Matlab/examples/SendDataInChunks.m
@@ -1,10 +1,11 @@
+addpath(genpath('..'))
 % instantiate the library
 disp('Loading library...');
 lib = lsl_loadlib();
 
 % make a new stream outlet
 disp('Creating a new streaminfo...');
-info = lsl_streaminfo(lib,'BioSemi','EEG',8,100,'cf_float32','sdfwerr32432');
+info = lsl_streaminfo(lib,'ninja','EEG',8,100,'cf_float32','sdfwerr32432');
 
 disp('Opening an outlet...');
 outlet = lsl_outlet(info);
@@ -13,5 +14,5 @@ outlet = lsl_outlet(info);
 disp('Now transmitting chunked data...');
 while true
     outlet.push_chunk(randn(8,50));
-    pause(0.5);
+    pause(5);
 end
