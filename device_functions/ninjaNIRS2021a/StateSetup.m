bcb63c4f89fa3ee84de3c435689d263008081c0a
Antonio Ortega <43629209+aortega255@users.noreply.github.com>
2023-01-11 16:43:38 -0500

    Update StateSetup.m

--- device_functions/ninjaNIRS2021a/StateSetup.m
+++ device_functions/ninjaNIRS2021a/StateSetup.m
@@ -1,4 +1,4 @@
-function result = StateSetup(sp,statefMap)
+function result = StateSetup(app,statefMap)
 %StateSetupS The purpose of this function is to setup the state of the
 %sources, detectors for the acquisition. Can be used to setup the
 %frequencies of the LEDs, or the power levels if they are fixed, or the
