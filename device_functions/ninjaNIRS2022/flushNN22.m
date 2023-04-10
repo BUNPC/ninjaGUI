6452c7c2ef339c73a858699132ba58b0f2262d23
Sreekanth Kura <sreekanth.kura7@gmail.com>
2023-04-10 12:02:49 -0400

    update some missing functions for new firmware and update snirf package

---
+++ device_functions/ninjaNIRS2022/flushNN22.m
@@ -0,0 +1,46 @@
+% Matlab helper functions for NN22_ControlBoard00
+% 
+% Initial version: 2023-3-4
+% Bernhard Zimmermann - bzim@bu.edu
+% Boston University Neurophotonics Center
+%
+% This function will flush serial data from all microcontrollers and the
+% FPGA
+
+function stat = flushNN22(sp,stat)
+
+
+% make sure system is not running
+stat.run = false;
+stat = updateStatReg(sp,stat);
+pause(0.1);
+
+% Reset program counters 
+sp.flush();
+stat.rst_pca = true;
+stat.rst_detb = true;
+stat = updateStatReg(sp,stat);
+
+pause(0.1);
+stat.rst_pca = false;
+stat.rst_detb = false;
+stat = updateStatReg(sp,stat, false);
+pause(0.6);
+
+% Redundant: also run a bit without sampling
+% (flush data out of tx buffers in all microcontrollers)
+ramb_tmp = stat.ramb;
+ramb_tmp(:,[9 10]) = 0; % reprog RAM B
+uploadToRAM(sp, ramb_tmp, 'b', false);
+stat.run = true;
+stat = updateStatReg(sp,stat);
+pause(0.1);
+stat.run = false;
+stat = updateStatReg(sp,stat);
+pause(0.1);
+sp.flush();
+pause(0.2);
+sp.flush();
+
+% Restore RAM B
+uploadToRAM(sp, stat.ramb, 'b', false);
