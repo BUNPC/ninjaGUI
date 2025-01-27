d4d71f83b9ab47d80aeba0fbc3160b5e58764db2
sreekanth kura <sreekanth.kura7@gmail.com>
2025-01-27 18:51:59 -0500

    add methods to do powerCalibration using two acquistions instead of 8

---
+++ device_functions/ninjaNIRS2024/createLEDPowerCalibrationSrcRAM_allPowerLevels.m
@@ -0,0 +1,40 @@
+function srcram = createLEDPowerCalibrationSrcRAM_allPowerLevels( SD)
+
+    srcram = zeros(7,1024,32);
+    srcram(:,:,21) = 1;
+    srcram(:,:,31) = 1; % using this bit as a hack for identifying source 0 when we look at 5 bits in mapToMeasurementList()
+    
+    lstS = unique(SD.MeasList(:,1));
+    
+    % loop over unique sources
+    % fill in odd states and leave even states as dark states
+    % alternate between two wavelengths for each state
+    
+    iState = 1;
+    
+    maxPower = round(logspace(2,log10(2^16-1),7));
+
+    for iS = 1:length(lstS)
+        iSrcMod = ceil((lstS(iS)-0.1)/8); 
+        iSrc = mod(lstS(iS)-1,8);
+        for iPower = 1:length(maxPower)
+            srcram(iSrcMod, iState, 1:16) = bitget(maxPower(iPower), range(0, 16));
+            srcram(iSrcMod, iState, 17:20) = bitget(iSrc*2, range(0, 4));
+            srcram(iSrcMod, iState, 21) = 0;
+            srcram(iSrcMod, iState, 31) = 0;
+            iState = iState + 1;
+        end
+
+        iState = iState + 1;
+        for iPower = 1:length(maxPower)
+            srcram(iSrcMod, iState, 1:16) = bitget(maxPower(iPower), range(0, 16));
+            srcram(iSrcMod, iState, 17:20) = bitget(iSrc*2+1, range(0, 4));
+            srcram(iSrcMod, iState, 21) = 0;
+            srcram(iSrcMod, iState, 31) = 0;
+            iState = iState + 1;
+        end
+        iState = iState + 1;
+    end
+    srcram(:,(iState-1):end,32) = 1; % mark sequence end
+end
+
---
+++ device_functions/ninjaNIRS2024/fastLEDPowerCalibration.m
@@ -0,0 +1,183 @@
+function fastLEDPowerCalibration( app, hAxes )
+
+stat = app.deviceInformation.stat; % DOES THIS WORK HERE? I need stat.s for initStat()
+
+if ~exist('hAxes')
+    reportSigLevel( app, 1 );
+    return
+end
+flagSpatialMultiplex = get(hAxes.cb,'value');
+
+% Get measList and Add rhoSDS to 5th column
+measList = app.nSD.measList;
+rhoSDS = zeros(size(measList,1),1);
+for iML = 1:size(measList,1)
+    iS = measList(iML,1);
+    iD = measList(iML,2);
+    rhoSDS(iML) = sum( (app.nSD.SrcPos3D(iS,:)-app.nSD.DetPos3D(iD,:)).^2 )^0.5;
+end
+measList(:,5) = rhoSDS;
+
+set( hAxes.txa_pow,'value',sprintf('doing power calibration...'));
+
+% Collect dark state
+srcram = createLEDPowerCalibrationSrcRAM( app.nSD, 0 );
+app.deviceInformation.srcram = srcram ;
+foo=find(srcram(1,:,32)==1);
+nStates=foo(1);
+app.deviceInformation.stateIndices = mapToMeasurementList(app.deviceInformation.srcram,measList);
+%submit to device
+rama = zeros(1024,32);
+rama(nStates:end,9) = 1; % mark sequence end
+
+uploadToRAM( stat, rama, 'a', false);
+for isrcb = 1:7
+    if stat.srcb_active(isrcb)
+        uploadToRAM(stat, squeeze(srcram(isrcb,:,:)), 'src', false, isrcb);
+    end
+end
+
+foldname=char(datetime('now','Format','y-MM-dd'));
+foldname2 = 'LEDPowerCalibration';
+if ~exist(['.\' foldname],'dir')
+    mkdir([foldname])
+end
+if ~exist(['.\' foldname '\' foldname2],'dir')
+    mkdir([foldname '\' foldname2])
+end
+
+fnameDark=sprintf('%s\\%s\\LEDPowerCalibration_a00_%s.bin',foldname,foldname2,fname);
+fileID=fopen(fnameDark,'w');
+app.fstreamID=fileID;
+
+% turn acquisition on
+app.deviceFunctions.Acquisition(app,'start');
+pause(1)
+%stop acquisition
+app.deviceFunctions.Acquisition(app,'stop');
+[data,~,~,~,~,~,~,dataDarkTmp,B] = app.deviceFunctions.ReadBytesAvailable(app);
+dataDark = mean(dataDarkTmp,2,'omitnan'); % really only need the lowest power level
+B_Dark = B;
+fclose(fileID);
+
+% collect for all other states
+srcram = createLEDPowerCalibrationSrcRAM_allPowerLevels( app.nSD);
+app.deviceInformation.srcram = srcram ;
+foo=find(srcram(1,:,32)==1);
+nStates=foo(1);
+%submit to device
+rama = zeros(1024,32);
+rama(nStates:end,9) = 1; % mark sequence end
+
+uploadToRAM( stat, rama, 'a', false);
+for isrcb = 1:7
+    if stat.srcb_active(isrcb)
+        uploadToRAM(stat, squeeze(srcram(isrcb,:,:)), 'src', false, isrcb);
+    end
+end
+
+fnameDark=sprintf('%s\\%s\\LEDPowerCalibration_a01_%s.bin',foldname,foldname2,fname);
+fileID=fopen(fnameDark,'w');
+app.fstreamID=fileID;
+% turn acquisition on
+app.deviceFunctions.Acquisition(app,'start');
+pause(1)
+%stop acquisition
+app.deviceFunctions.Acquisition(app,'stop');
+srcPowerLowHigh = ones(8 * 7, len(app.nSD.MeasList), 2, 'uint8')
+for iPower = 1:7
+    [data,~,~,~,~,~,~,dataDarkTmp,B] = app.deviceFunctions.ReadBytesAvailable(app);
+    if iPower == 1
+        Bpow = zeros(size(B,2),size(B,3),7);
+        dataLEDPowerCalibration = zeros(size(dataDarkTmp,2),7);
+    end
+    dataLEDPowerCalibration(:,iPower) = squeeze(mean(data,2,'omitnan'));
+    Bpow(:,:,iPower) = squeeze( mean(B,1,'omitnan') );
+end
+fclose(fileID);
+
+set( hAxes.txa_pow,'value',sprintf('Plotting Results...'));
+%close(hWait)
+
+disp('LED Power Calibration Data Acquisition Finished')
+
+% if acquired successfully
+% return state to default
+%app.deviceFunctions.StateSetup(app,[]);
+
+%if isfield(app.nSD,'freqMap')
+%    app.deviceFunctions.StateSetup(app,app.nSD.freqMap);
+%else
+%    app.deviceFunctions.StateSetup(app,[]);
+%end
+
+% return fID to default
+app.fstreamID=standardfID;
+
+% return the subtract dark flag to whatever it should be
+if app.SubtractdarkMenu.Checked
+    app.deviceInformation.subtractDark=1;
+else
+    app.deviceInformation.subtractDark=0;
+end
+
+% dual power cycling or not
+thresholds = app.deviceInformation.levelRepresentation.thresholds;
+
+% dual power setting
+
+% Dual Power Cycling
+% optimize power levels and update stateMap and stateIndices
+app.deviceInformation.dataLEDPowerCalibration = dataLEDPowerCalibration;
+
+% need to create srcPowerLowHigh(nSrc,nDet,nWav)
+SD = app.nSD;
+SD.sds_range = str2num( get(hAxes.sds,'value') );
+
+[srcram, stateIndices, optPowerLevel, srcPowerLowHigh, dSig, srcModuleGroups] = LEDPowerCalibration_dualLevels(SD,dataLEDPowerCalibration,thresholds,flagSpatialMultiplex);
+app.deviceInformation.srcram = srcram;
+app.deviceInformation.stateIndices = stateIndices;
+app.deviceInformation.optPowerLevel = optPowerLevel;
+app.deviceInformation.srcPowerLowHigh = srcPowerLowHigh;
+app.deviceInformation.dSig = dSig;
+app.deviceInformation.srcModuleGroups = srcModuleGroups;
+app.deviceInformation.flagSpatialMultiplex = flagSpatialMultiplex;
+nSD = app.nSD;
+save('dualPowerStateMapandIndices.mat','srcram','stateIndices','optPowerLevel','srcPowerLowHigh','dSig','Bpow','nSD','thresholds')
+
+% check if source multiplexing
+flagSpatialMultiplex = 0;
+for ii=1:length(srcModuleGroups)
+    if length(srcModuleGroups(ii))>1
+        flagSpatialMultiplex = 1;
+    end
+end
+set(hAxes.cb,'value',flagSpatialMultiplex);
+
+foo=find(srcram(1,:,32)==1);
+nStates=foo(1);
+
+rama = zeros(1024,32);
+rama(nStates:end,9) = 1; % mark sequence end
+uploadToRAM(stat, rama, 'a', false);
+for isrcb = 1:7
+    if stat.srcb_active(isrcb)
+        uploadToRAM(stat, squeeze(srcram(isrcb,:,:)), 'src', false, isrcb);
+    end
+end
+stat.rama = rama;
+stat.srcram = srcram;
+
+
+% update rate
+foo=find(srcram(1,:,32)==1);
+nStates=foo(1);
+fs=app.deviceInformation.stat.state_fs / nStates; 
+app.deviceInformation.Rate=fs;
+app.editRate.Value=app.deviceInformation.Rate;
+
+%%
+% report
+reportSigDark( app.nSD, dataDark, B_Dark, hAxes );
+reportSigVsSDS( measList(:,1:4), rhoSDS, dSig, hAxes );
+set( hAxes.txa_pow,'value',sprintf(''));
